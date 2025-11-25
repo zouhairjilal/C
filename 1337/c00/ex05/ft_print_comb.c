2495d511e5aff6215b00e6e62d2c833e29a1ec05
zouhairjilal <55323285+zouhairjilal@users.noreply.github.com>
2025-11-25 23:58:02 +0100

    add

---
+++ 1337/c00/ex05/ft_print_comb.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void	ft_print_num(int i)
+{
+	i += '0';
+	write(1, &i, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				ft_print_num(a);
+				ft_print_num(b);
+				ft_print_num(c);
+				if (!(a == 7 && b == 8 && c == 9))
+					write(1, ", ", 2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
